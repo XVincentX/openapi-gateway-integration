f1e06f1d65be5e6ba275926ff6a127d13e1b25ae
Glitch (express-gateway-test) <none>
2017-12-12 15:05:40 +0000

    🐁😄 Checkpoint server.js:2928 config/gateway.config.yml:2928 README.mD:2928 README.md:2928

---
+++ README.md
@@ -0,0 +1,12 @@
+# E
+
+## What's up?
+
+This is a sample [Express Gateway](https://express-gateway.io) instance deployed on
+Glitch showing how you can DRY your configuration files using references in YAML
+files.
+
+It is configured with couple of policies (`jwt`, `oauth2`) to secure some endpoints
+and the Admin API is (insecurely) exposed on the web.
+
+_Enjoy it_
--- server.js
+++ server.js
@@ -25,7 +25,7 @@ credentialService.insertScopes(['read', 'write'])
   .then(credential => Object.assign(appCredential, credential))
   .then((credential) => {
     console.table('Application credentials',[credential]);
-    gateway().load(path.join(__dirname, 'config')).run()
+    gateway().load(path.join(__dirname, 'config')).run();
   });
 
 // Sample token: eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik1rVkJSa0ZFTkVGRVJVRXdNVVEyTURrNE1EazJNRVJETWpCR05FUTVPREEyTnpFeVF6QkZSUSJ9.eyJpc3MiOiJodHRwczovL2ZldGlzaC5hdXRoMC5jb20vIiwic3ViIjoiUnA1RzEyYktpWTlrSWJZMFZkdmFzYklrQk9kNmJHcUtAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vZmV0aXNoLmF1dGgwLmNvbS9hcGkvdjIvIiwiaWF0IjoxNTEzMDgyNDQwLCJleHAiOjE1MTMxNjg4NDAsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.XNKOEnZuO_2ze4B9IePb3LANE8bjT6LQ1-bRrl0Ka2nDY3Z3ppi5wEbj7pqrdJ7dquzvmIdjQl3YwaamVbkuK5Myr6UGSQ1954syJx2GpWFtwoLZsdO3DDdpSmixYmUtpZG9MaEZ8Kx84cKBlmAje38qNfKn-40Ar72qTCYLXhpW-lIe7LbdCnd-0vB4WzAozm7wgfS05kjf6KGTcf5IbKhazbAD_wlugDIIbCo1Ewv6_uTTPYy8FiRlfuIBz1VMAzH8r2EDdqBI2tjUwfRC1X3ZY13Dr4vJ93YBQbB6cjb-dbZgCOTlPVsxjGuHmnfiQDtNIPKxb7pM02QrgifnBQ
