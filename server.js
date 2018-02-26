4a30ce87cbf1fd7fcbf83d8e5718ca3d34944b98
Glitch (express-gateway-test) <none>
2018-02-26 15:05:43 +0000

    📟🐂 Checkpoint config/gateway.config.yml:2928/83 server.js:2928/605

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
+require('console.table');
 const path = require('path');
 const gateway = require('express-gateway');
+const services = require('express-gateway/lib/services');
+
+services.user.insert(
+{
+	"username": "vncz",
+	"firstname": "Vincenzo",
+	"lastname": "Chianese",
+	"email": "test@foo.com"
+}).then((user)=> {
+  console.table([user]);
+  return services.application.insert({
+	"name": "my-app",
+	"redirectUri": "http://example.com"
+  }, user.id);
+}).then((app)=>{
+  console.table([app])
+  
+}).then(()=>{
+  gateway().load(path.join(__dirname, 'config')).run();
+});
 
 
-gateway().load(path.join(__dirname, 'config')).run();
