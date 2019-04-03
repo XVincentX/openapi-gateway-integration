b3c6e008b3ad69806e90fe4074acf6d15f08f8c2
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-04-03 14:17:52 +0200

    remove stuff

---
+++ send-request.sh
@@ -0,0 +1,4 @@
+curl -X POST -H 'Content-Type: application/json' -d '{"name":"Puppy", "type":"dog"}' https://egpetstore.glitch.me/pets
+curl https://egpetstore.glitch.me/pets
+
+curl -X POST -H 'Content-Type: application/json' -d '{"name":"Puppy", "type":"mouse"}' https://egpetstore.glitch.me/pets
