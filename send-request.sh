1fc3be529319bbd144351f3c618d26ce64eb678c
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-04-04 11:32:37 +0200

    new lines

--- send-request.sh
+++ send-request.sh
@@ -1,5 +1,7 @@
 curl -X POST -H 'Content-Type: application/json' -d '{"name":"Puppy", "type":"dog"}' https://egpetstore.glitch.me/pets
+echo
 curl -X POST -H 'Content-Type: application/json' -d '{"name":"Puppy", "type":"dog", "favoriteFood":"croquetas"}' https://egpetstore.glitch.me/pets
+echo
 curl https://egpetstore.glitch.me/pets
-
+echo
 curl -X POST -H 'Content-Type: application/json' -d '{"name":"Puppy", "type":"mouse"}' https://egpetstore.glitch.me/pets
